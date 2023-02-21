4fb4a77c430632622ceea4b7b8ca07ff9cd65410
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2023-02-21 20:45:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function otpless() {
+
+    const waId = otplessWaId(whatsapp-login);
+    
+    // Once you signup/sign a user, you can redirect the user to your signup/signin flow.
+    
+    window.open("https://demo.com/home.html", "_self")
+    
+    }
